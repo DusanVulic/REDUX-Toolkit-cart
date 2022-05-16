684012fba8ead4e9b3dfe63c40bd21db4256a0c2
Dusan <dusanvulic11@gmail.com>
2022-05-16 14:52:26 +0200

    remove item ,increase, decrease functions added

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -21,13 +21,22 @@ const cartSlice = createSlice({
             const itemId = action.payload;
             state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
         },
+
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
     },
 });
 
 console.log(cartSlice);
 
 // export reducer funkcije !!!
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 //exportujem reducer iz cart Slice
 export default cartSlice.reducer;
