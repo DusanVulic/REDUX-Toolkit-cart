48743108a4cefc624c09d6139620648db4cee342
Dusan <dusanvulic11@gmail.com>
2022-05-16 15:44:54 +0200

    modal slice added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from "react-redux";
 
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((store) => store.cart);
   useEffect(() => {
     dispatch(calculateTotals());
+    // eslint-disable-next-line
   }, [cartItems]);
 
   const dispatch = useDispatch();
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
