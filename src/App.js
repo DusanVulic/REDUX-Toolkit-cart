995ba6dacc34cb0aafaff33f0e0af0c3b3cf832b
Dusan <dusanvulic11@gmail.com>
2022-05-16 15:29:15 +0200

    total created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 
+import { useSelector, useDispatch } from "react-redux";
+
+import { calculateTotals } from "./features/cart/cartSlice";
+
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  const dispatch = useDispatch();
   return (
     <main>
       <Navbar />
